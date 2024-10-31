ad07cab3973cb9da1dfc41116e10789a33a6de8e
Liz K <82458656+lizknuty@users.noreply.github.com>
2024-10-31 23:55:41 +0000

    updated prisma.ts

--- sewindieapp/src/lib/prisma.ts
+++ sewindieapp/src/lib/prisma.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient();
-  }
-  prisma = (global as any).prisma;
+declare global {
+  // Allow global `prisma` for hot-reloading in development to avoid multiple Prisma client instances
+  var prisma: PrismaClient | undefined;
 }
 
+// Define the Prisma client with a specific type
+const prisma: PrismaClient = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+
 export default prisma;
+
